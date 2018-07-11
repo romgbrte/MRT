85d44f4b6363b7491e6c44fe663a542c2cbc94ff
Robert Rabel <robrte@hotmail.com>
2018-07-11 12:33:22 -0500

    Bootstrap 4, Fontawesome 5, TODO

--- MRT/App_Start/BundleConfig.cs
+++ MRT/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace MRT
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,11 +20,9 @@ namespace MRT
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-materia.min.css",
+                      "~/Content/fontawesome/css/all.css",
                       "~/Content/site.css"));
         }
     }
