3812dc6dcdcd522c5a94ed96739b0e894fcc3d84
Robert Rabel <robrte@hotmail.com>
2018-07-27 14:42:55 -0500

    Custom Http error pages

--- MRT/App_Start/FilterConfig.cs
+++ MRT/App_Start/FilterConfig.cs
@@ -7,7 +7,6 @@ namespace MRT
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
             filters.Add(new RequireHttpsAttribute());
         }
---
+++ MRT/Controllers/ErrorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRT.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return View();
+        }
+
+        public ActionResult Internal()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return View();
+        }
+
+        public ActionResult AccessDenied()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return View();
+        }
+    }
+}
