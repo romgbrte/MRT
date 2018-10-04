3a14e532cbfae4af3b6cb95a903f618463d8bd98
Robert Rabel <robrte@hotmail.com>
2018-10-04 12:47:24 -0500

    Additional sql seed files

--- MRT/DataContexts/DataMigrations/201808032230360_SeedCarriers.cs
+++ MRT/DataContexts/DataMigrations/201808032230360_SeedCarriers.cs
@@ -1,16 +1,31 @@
 namespace MRT.DataContexts.DataMigrations
 {
     using System;
+    using System.IO;
     using System.Data.Entity.Migrations;
     
     public partial class SeedCarriers : DbMigration
     {
         public override void Up()
         {
+            // Remember to set the target .sql file as an Embedded Resource via its Properties
+            var sqlPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.IndexOf("bin")),
+                "SeedData",
+                @"dbo.Carriers.data.sql"
+            );
+            Sql(File.ReadAllText(sqlPath));
+
+            /*
             Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Great Big West Mutual', 4.25)");
             Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('New Vegas Assurances', 7.77)");
             Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Geneva Financial Group', 2.20)");
             Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('United Assurance Collective', 6.67)");
+            Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Atlas Global Union', 7.65)");
+            Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Southwest Coverage Specialists', 11.75)");
+            Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Firefox Nationwide', 5.425)");
+            Sql("INSERT INTO Carriers (Name, BaseRate) VALUES ('Northeast Coverage Unlimited', 9.5)");
+            */
         }
 
         public override void Down()
---
+++ MRT/SeedData/dbo.Carriers.data.sql
@@ -0,0 +1,10 @@
+SET IDENTITY_INSERT [dbo].[Carriers] ON
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (1, N'Great Big West Mutual', 4.25)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (2, N'New Vegas Assurances', 7.77)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (3, N'Geneva Financial Group', 2.25)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (4, N'United Assurance Collective', 6.67)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (5, N'Atlas Global Union', 7.65)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (6, N'Southeast Coverage Specialists', 11.75)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (7, N'Firefox Nationwide', 5.425)
+INSERT INTO [dbo].[Carriers] ([Id], [Name], [BaseRate]) VALUES (8, N'Northeast Coverage Unlimited', 9.5)
+SET IDENTITY_INSERT [dbo].[Carriers] OFF
